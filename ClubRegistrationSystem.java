c02d93cee236a9d80fb0940420cb82a0116f80e0
Istiaq-hossain-shawon <hossain.ict@gmail.com>
2026-08-14 03:19:12 +1200

    add ClubRegistrationSystem class

---
+++ ClubRegistrationSystem.java
@@ -0,0 +1,56 @@
+ 
+public class ClubRegistrationSystem {
+
+    public static final String AUCKLAND = "Auckland";
+    public static final String PALMERSTON_NORTH = "PN";
+    public static final String ONLINE = "Online";
+
+    private static final int MAX_ACTIVITIES = 20;
+    private static final int MAX_STUDENTS = 50;
+
+    private String systemName;
+    private Activity[] activities;      
+    private int activityCount;
+    private Student[] students;         
+    private int studentCount;
+
+    public ClubRegistrationSystem(String systemName) {
+        this.systemName = systemName;
+        this.activities = new Activity[MAX_ACTIVITIES];
+        this.activityCount = 0;
+        this.students = new Student[MAX_STUDENTS];
+        this.studentCount = 0;
+    }
+
+    // Getter and setter methods
+    public String getSystemName() {
+        return systemName;
+    }
+
+    public void setSystemName(String systemName) {
+        this.systemName = systemName;
+    }
+
+    public int getActivityCount() {
+        return activityCount;
+    }
+
+    public int getStudentCount() {
+        return studentCount;
+    }
+
+    public Activity getActivity(int index) {
+        if (index < 0 || index >= activityCount) {
+            return null;
+        }
+        return activities[index];
+    }
+
+    public Student getStudent(int index) {
+        if (index < 0 || index >= studentCount) {
+            return null;
+        }
+        return students[index];
+    }
+ 
+}
