67d53f0cd7aeb46ff8a7f6cc0676ef89ae79f6cc
Istiaq-hossain-shawon <hossain.ict@gmail.com>
2026-08-14 07:43:10 +1200

    Add addActivities() to load six test activities

--- Main.java
+++ Main.java
@@ -14,5 +14,37 @@ public class Main {
 
          
     }
- 
+
+    // Adds the six activities of the test data to the system
+    private static void addActivities(ClubRegistrationSystem system) {
+        String auckland = ClubRegistrationSystem.AUCKLAND;
+        String pn = ClubRegistrationSystem.PALMERSTON_NORTH;
+        String online = ClubRegistrationSystem.ONLINE;
+
+        system.addActivity(new Activity("A101", "Basketball Club",
+                new String[]{"Sport"}, "Wednesday PM",
+                true, true, new String[]{auckland, pn}));
+
+        system.addActivity(new Activity("A102", "Coding Circle",
+                new String[]{"Technology"}, "Saturday AM",
+                true, false, new String[]{auckland, pn, online}));
+
+        system.addActivity(new Activity("A103", "Choir Group",
+                new String[]{"Art"}, "Monday PM",
+                false, false, new String[]{auckland, pn}));
+
+        system.addActivity(new Activity("A104", "Robotics Workshop",
+                new String[]{"Technology"}, "Friday PM",
+                false, true, new String[]{auckland, pn}));
+
+        system.addActivity(new Activity("A105", "Creative Writing Club",
+                new String[]{"Literature"}, "Thursday PM",
+                true, false, new String[]{auckland, pn, online}));
+
+        system.addActivity(new Activity("A106", "Hiking Group",
+                new String[]{"Sport", "Lifestyle"}, "Sunday AM",
+                true, false, new String[]{auckland, pn}));
+    }
+
+     
 }
